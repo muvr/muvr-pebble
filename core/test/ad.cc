2ac24ce4756ee3e24840b9c9cae5655605c81c6e
Jan Machacek <jan.machacek@gmail.com>
2015-04-14 16:15:20 +0100

    Tests

--- core/test/ad.cc
+++ core/test/ad.cc
@@ -1,48 +1,43 @@
 #include <gtest/gtest.h>
 #include "ad.h"
-#include "instances.h"
+#include "mocks.h"
 
 using namespace pebble;
 
-class gfs_test : public testing::Test {
+class ad_test : public testing::Test {
 protected:
     static uint8_t *buffer;
     static uint16_t size;
 public:
-    static void gfs_callback(uint8_t *b, uint16_t s);
+    static void ad_callback(uint8_t *b, uint16_t s);
 
-    virtual ~gfs_test();
+    virtual ~ad_test();
 };
 
-gfs_test::~gfs_test() {
+ad_test::~ad_test() {
    if (buffer != nullptr) free(buffer);
 }
 
-void gfs_test::gfs_callback(uint8_t *b, uint16_t s) {
+void ad_test::ad_callback(uint8_t *b, uint16_t s) {
     if (buffer != nullptr) free(buffer);
     buffer = (uint8_t *)malloc(s);
     memcpy(buffer, b, s);
     size = s;
 }
 
-uint8_t *gfs_test::buffer;
-uint16_t gfs_test::size;
+uint8_t *ad_test::buffer;
+uint16_t ad_test::size;
 
-TEST_F(gfs_test, Version1) {
-    // we have 12000B for our buffer, which means space for 2384 samples
-    // the accelerometer is configured to receive multiple of 8 number
-    // if AccelRawData values.
-
-    // that is, we must push 376 * 8 packets of accel data
+TEST_F(ad_test, overflows) {
     std::vector<AccelRawData> mock_data;
     AccelRawData a = { .x = 1000, .y = 5000, .z = -5000 };
     for (int i = 0; i < 2; i++) mock_data.push_back(a);
 
-    ad_start(gfs_test::gfs_callback, GFS_SAMPLING_100HZ);
-    for (int i = 0; i < 126; i++) instances::accel_service << mock_data;
+    ad_start(ad_test::ad_callback, GFS_SAMPLING_100HZ);
+    for (int i = 0; i < 126; i++) *mocks::accel_service() << mock_data;
 
-    ASSERT_TRUE(gfs_test::buffer != nullptr);
-    threed_data *data = reinterpret_cast<threed_data *>(gfs_test::buffer);
+    ASSERT_TRUE(ad_test::buffer != nullptr);
+    threed_data *data = reinterpret_cast<threed_data *>(ad_test::buffer);
     for (int i = 0; i < 126; i++) {
         EXPECT_EQ(data[i].x_val, 1000);
         EXPECT_EQ(data[i].y_val, 4095);
--- core/test/am.cc
+++ core/test/am.cc
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include "am.h"
-#include "instances.h"
+#include "mocks.h"
 
 class am_test : public testing::Test {
 
@@ -10,5 +10,6 @@ TEST_F(am_test, empty) {
     auto callback = am_start(123, 100, 5);
     uint8_t buf[] = {1, 2, 3};
     callback(buf, 3);
-    pebble::instances::app_messages.last_dict();
+    auto data = pebble::mocks::app_messages()->last_dict().get<std::vector<uint8_t>>(0xface0fb0);
+    for (auto &x : data) std::cout << std::to_string(x) << std::endl;
 }
