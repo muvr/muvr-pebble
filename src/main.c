5c0e309820aaef6842ddb49c9280f84e33368530
Jan Machacek <jan.machacek@gmail.com>
2015-05-02 17:07:28 +0100

    Better UI

--- src/main.c
+++ src/main.c
@@ -1,75 +1,49 @@
 #include "pebble.h"
 #include "../core/main/ad.h"
 #include "../core/main/am.h"
+#include "resistance_exercise_layer.h"
 
-#define TIMER_MS 1000
+static struct {
+    Window *rex_window;
+    message_callback_t message_callback;
+} main_ctx;
 
-static Window *window;
-
-static GRect window_frame;
-
-static Layer *disc_layer;
-
-static AppTimer *timer;
-
-#define TEXT_LENGTH 150
-
-static void disc_layer_update_callback(Layer *me, GContext *ctx) {
-    graphics_context_set_text_color(ctx, GColorWhite);
-    GRect bounds = layer_get_frame(me);
-
-    char text[TEXT_LENGTH];
-
-    am_get_status(text, TEXT_LENGTH);
-
-    graphics_draw_text(ctx,
-            text,
-            fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD),
-            GRect(5, 5, bounds.size.w - 10, 100),
-            GTextOverflowModeWordWrap,
-            GTextAlignmentLeft,
-            NULL);
+static void accepted(const uint8_t index) {
+    ad_start(main_ctx.message_callback, AD_SAMPLING_50HZ, 2050);
+    rex_not_moving();
 }
 
-static void timer_callback(void *data) {
-    layer_mark_dirty(disc_layer);
-    timer = app_timer_register(TIMER_MS, timer_callback, NULL);
+static void rejected(void) {
+    ad_start(main_ctx.message_callback, AD_SAMPLING_50HZ, 2050);
+    rex_not_moving();
 }
 
-static void window_load(Window *window) {
-    Layer *window_layer = window_get_root_layer(window);
-    GRect frame = window_frame = layer_get_frame(window_layer);
-
-    disc_layer = layer_create(frame);
-    layer_set_update_proc(disc_layer, disc_layer_update_callback);
-    layer_add_child(window_layer, disc_layer);
-}
-
-static void window_unload(Window *window) {
-    layer_destroy(disc_layer);
+static void timed_out(const uint8_t index) {
+    ad_start(main_ctx.message_callback, AD_SAMPLING_50HZ, 2050);
+    rex_not_moving();
 }
 
 static void init(void) {
-    srand(time(NULL));
-    window = window_create();
-    window_set_window_handlers(window, (WindowHandlers) {
-            .load = window_load,
-            .unload = window_unload
-    });
-    window_stack_push(window, true /* Animated */);
-    window_set_background_color(window, GColorBlack);
+    main_ctx.rex_window = rex_init();
+    window_stack_push(main_ctx.rex_window, true);
 
-    ad_start(am_start(0xad, 50, 5), AD_SAMPLING_50HZ, 2050);
-//    ad_start(NULL, AD_SAMPLING_50HZ, 2000);
+    main_ctx.message_callback = am_start(0xad, 50, 5);
 
-    timer = app_timer_register(TIMER_MS, timer_callback, NULL);
+    //ad_start(main_ctx.message_callback, AD_SAMPLING_50HZ, 2050);
+#if 1
+    resistance_exercise_t x[] = {
+       {.name = "Bicep curl",   .repetitions = 10, .weight = 20},
+       {.name = "Tricep press", .repetitions = 10, .weight = 25}
+    };
+    rex_classification_completed(x, 2, accepted, timed_out, rejected);
+#endif
 }
 
 static void deinit(void) {
     ad_stop();
     am_stop();
 
-    window_destroy(window);
+    window_destroy(main_ctx.rex_window);
 }
 
 int main(void) {
---
+++ src/resistance_exercise_layer.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Repetition count
+typedef uint8_t repetitions_t;
+
+/// Intensity in 0..100
+typedef uint8_t intensity_t;
+
+/// Classification confidence in 0..100
+typedef uint8_t confidence_t;
+
+/// Weight in user units (converted on the mobile). Beyond mobile, everything is in the metric system
+typedef uint16_t weight_t;
+
+#define UNK_REPETITIONS 255
+#define UNK_INTENSITY 255
+#define UNK_WEIGHT 65535
+
+typedef struct {
+    char name[64];
+    confidence_t  confidence;       // 0..100
+    repetitions_t repetitions;      // 1..~50,  UNK_REPETITIONS for unknown
+    intensity_t   intensity;        // 1..100,  UNK_INTENSITY for unknown
+    weight_t      weight;           // 1..~500, UNK_WEIGHT for unknown
+} resistance_exercise_t;
+
+///
+/// Callback that will be called when the user confirms the exercise
+///
+typedef void (*classification_accepted_callback_t)(const uint8_t index);
+
+///
+/// Callback that will be called when the user rejects the exercise
+///
+typedef void (*classification_rejected_callback_t)(void);
+
+///
+/// Callback that will be called when the user just leaves the selection
+///
+typedef void (*classification_timedout_callback_t)(const uint8_t index);
+
+///
+/// Display the resistance_exercise examples
+///
+void rex_classification_completed(resistance_exercise_t *exercises, uint8_t count,
+                                  classification_accepted_callback_t accepted,
+                                  classification_timedout_callback_t timed_out,
+                                  classification_rejected_callback_t rejected);
+
+void rex_moving(void);
+
+void rex_exercising(void);
+
+void rex_not_moving(void);
+
+Window* rex_init(void);
+
+#ifdef __cplusplus
+}
+#endif
