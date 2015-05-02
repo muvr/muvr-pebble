de0d00ab732a04c295da0fe378ef2d80ebe65bba
Jan Machacek <jan.machacek@gmail.com>
2015-05-02 17:41:09 +0100

    Feedback

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <pebble.h>
 #include "pebble.h"
 #include "../core/main/ad.h"
 #include "../core/main/am.h"
@@ -26,14 +27,39 @@ static void timed_out(const uint8_t index) {
     rex_not_moving();
 }
 
+static void app_message_received(DictionaryIterator *iterator, void *context) {
+    Tuple *t = dict_read_first(iterator);
+    while (t != NULL) {
+        switch (t->key) {
+            case 0xa0000000: // not moving
+                rex_not_moving();
+                return;
+            case 0xa0000001: // moving
+                rex_moving();
+                return;
+            case 0xa0000002: // exercising
+                rex_exercising();
+                return;
+            case 0xa0000003: // classified
+                ad_stop();
+                resistance_exercise_t *res = (resistance_exercise_t*)t->value->data;
+                uint8_t count = t->length / sizeof(resistance_exercise_t);
+
+                rex_classification_completed(res, count, accepted, timed_out, rejected);
+                return;
+        }
+        t = dict_read_next(iterator);
+    }
+}
+
 static void init(void) {
+    app_message_register_inbox_received(app_message_received);
     main_ctx.rex_window = rex_init();
     window_stack_push(main_ctx.rex_window, true);
 
     main_ctx.message_callback = am_start(0xad, 50, 5);
-
-    //ad_start(main_ctx.message_callback, AD_SAMPLING_50HZ, 2050);
-#if 1
+    ad_start(main_ctx.message_callback, AD_SAMPLING_50HZ, 2050);
+#if 0
     resistance_exercise_t x[] = {
        {.name = "Bicep curl",   .repetitions = 10, .weight = 20},
        {.name = "Tricep press", .repetitions = 10, .weight = 25}
