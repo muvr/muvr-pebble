fd3e466ee8614804bc86148693ad252a7c4ca2f3
Jan Machacek <jan.machacek@gmail.com>
2015-09-12 10:09:42 +0100

    Removed dups

--- src/main.c
+++ src/main.c
@@ -181,17 +181,6 @@ static void init(void) {
     main_ctx.vibes_start_time = 0;
 
     main_ctx.message_callback = am_start(0xad, 50, 5);
-    
-#if 0
-    resistance_exercise_t x[] = {
-       {.name = "Bicep curl",   .repetitions = 10, .weight = 20},
-       {.name = "Tricep extension", .repetitions = 10, .weight = 25}
-    };
-    rex_set_current(x);
-    //rex_classification_completed(x, 2, accepted, timed_out, rejected);
-#else
-    //start(NULL);
-#endif
 }
 
 static void deinit(void) {
